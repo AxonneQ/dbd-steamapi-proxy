45b0fcc83ea24ee47ca652e6d1fdbad5e1694eab
AxonneQ <bolek.igor.94@gmail.com>
2020-06-07 16:50:09 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # dbd-steamapi-proxy
+
+This is a simple proxy server that fetches Dead By Daylight statistics and total online player count.
+
+#### Available Calls:
+1. `/?steamid=<INSERT STEAMID64>` - fetches a JSON file containing game statistics for given steamid profile*. 
+
+    *Note: Game details setting on a given profile must be public in order to get the stats. Otherwise `500 Internal Server Error` status will be received from Steam API.
+
+2. `/?getPlayers` - fetches total amount of online players currently playing Dead By Daylight. Only players who are logged in to Steam are taken into account.
+
+#### How to run:
+1. Install [Node.js](https://nodejs.org/en/download/). Works with version 13.12.0. Should work with newest.
+2. Clone/download this repository.
+3. Get Steam API key (if you don't have one, get one [here](https://steamcommunity.com/dev/apikey)) and add it to your environment variables. Name it `STEAMAPIKEY`, and set it's value to your Steam API key and restart your commandline to detect changes.
+4. Get make a SSL key and certificate, you can self sign it using openssl [Linux](https://www.openssl.org/source/) or [Windows](https://slproweb.com/products/Win32OpenSSL.html). Use the following command in the root directory of this project: `openssl req -nodes -x509 -keyout ssl/server.key -out ssl/server.cert`. You can also sign it using certified SSL services like Let's Encrypt.
+5. Once everything is done, run `npm start <port>` to start the server on desired port, else it will default to `11059`.
+
+#### How to use:
+Once the server is running, you can request a https request from your website/service using one of the available calls. e.g.
+`https://<server-ip>:<server-port>/?getPlayers`
+
---
+++ start.js
@@ -0,0 +1,59 @@
+const https = require("https");
+const port = process.argv[2] || 11059; //node start.js <?port>
+const url = require("url");
+const fs = require("fs");
+
+const totalPlayersApi = "https://api.steampowered.com/ISteamUserStats/GetNumberOfCurrentPlayers/v1/?appid=381210";
+const playerStatsApi = `https://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?appid=381210&key=${process.env.STEAMAPIKEY}&steamid=`;
+
+const options = {
+    key: fs.readFileSync("ssl/server.key"),
+    cert: fs.readFileSync("ssl/server.cert"),
+};
+
+var server = https.createServer(options, (s_req, s_res) => {
+    var client_ip = s_req.socket.remoteAddress;
+    var client_port = s_req.socket.remotePort;
+    var steamid = url.parse(s_req.url, true).query.steamid;
+    var players = url.parse(s_req.url, true).query.getPlayers;
+    var requestApi;
+
+    if (steamid !== undefined) {
+        requestApi = `${playerStatsApi}${steamid}`;
+        console.log(`Incoming connection from: ${client_ip}:${client_port}, requested player stats for ${steamid}`);
+    } else if (players !== undefined) {
+        requestApi = totalPlayersApi;
+        console.log(`Incoming connection from: ${client_ip}:${client_port}, requested total online players`);
+    } else {
+        s_res.statusCode = 400;
+        s_res.end();
+        return;
+    }
+
+    https.get(requestApi, (res) => {
+        res.on("error", (err) => {
+            console.error(err);
+            s_res.statusCode = 400;
+            s_res.end();
+        });
+
+        if (res.statusCode === 500) {
+            console.log("500 Internal Server Error.");
+            s_res.statusCode = 500;
+            s_res.end();
+        } else {
+            console.log("Retrieved player data.");
+            s_res.setHeader("content-type", "application/json; charset=utf-8");
+            res.pipe(s_res, { end: true });
+            console.log("Player data response sent.");
+        }
+    });
+
+    s_req.on("close", () => {
+        console.log(`Closed connection with ${s_req.socket.remoteAddress}:${s_req.socket.remotePort}\n`);
+    });
+});
+
+server.listen(port, () => {
+    console.log(`Listening on port: ${port}`);
+});
